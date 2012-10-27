dc3dbfba3f532fbaa31a65449d4984e40f8f4b43
ewoudj <ewoudj@gmail.com>
2012-10-27 16:39:05 +0200

    Authentication related changes, ajax module added

---
+++ config.js
@@ -0,0 +1,28 @@
+var config = {
+    debug: true,
+    debugUser: {
+        "token": "1234567890abcdef",
+        "secret": "1234567890abcdef",
+        "id": "1234567890abcdef",
+        "data": {
+            "id": "1234567890abcdef",
+            "name": "developer"
+        },
+        "service": "debug"
+    },
+    identityProviders: [{
+        service: "github",
+        id: "1234567890abcdef",
+        secret: "1234567890abcdef"
+    }, {
+        service: "google",
+        id: "1234567890abcdef",
+        secret: "1234567890abcdef"
+    }, {
+        service: "twitter",
+        id: "1234567890abcdef",
+        secret: "1234567890abcdef"
+    }]
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,45 +1,76 @@
 var connect = require('connect');
 var socketio = require('socket.io');
-var engine = require("./static/laserwar/engine").engine;
-var rules = require("./static/laserwar/rules").rules;
-var questions = Buffer(
-    "<html>" +
-        "<body style='font: 300% sans-serif'>" +
-            "<div><a href='/auth/github'>Who am I on Github?</a></div>" +
-            "<div><a href='/auth/google'>Who am I on Google?</a></div>" +
-            "<div><a href='/auth/twitter'>Who am I on Twitter?</a></div>" +
-        "</body>" +
-    "</html>"
-);
+var engine = require('./static/laserwar/engine').engine;
+var rules = require('./static/laserwar/rules').rules;
+var authom = require('authom');
+var config = require('./config');
+var ajax = require('./lib/ajax');
+
+try{
+    var configLocal = require('./config.local');
+    if(configLocal){
+        config = configLocal;
+    }
+}
+catch(exc){
+    // Ignore missing local config file
+}
 
 var server = connect()
-	.use(function(req, res, next){
-		console.log(req.originalUrl);
-		next();
-	})
+//	.use(function(req, res, next){
+//		console.log(req.originalUrl);
+//		next();
+//	})
     .use(connect.cookieParser())
     .use(connect.session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+    .use(connect.bodyParser())
     .use(function(req, res, next){
-        if(req.originalUrl.indexOf("/editor/") == 0 && !req.session.hasIndentity){
-//            res.writeHead(200, {
-//                "Content-Type": "text/html",
-//                "Content-Length": questions.length
-//            });
-//            res.end(questions);
-            req.url = req.originalUrl = "/editor/signin.html";
-            next();
+        if(config.debug && !req.session.hasIndentity){
+            req.session.hasIndentity = true;
+            req.session.identity = config.debugUser;
+        }
+        if(req.originalUrl.indexOf('/editor/') == 0 && !req.session.hasIndentity){
+            res.writeHead(302, {
+                'Location': '/signin.html'
+            });
+            res.end();
         }
         else {
             next();
         }
     })
+    .use(ajax({'/editor/behaviour': {
+        getUserDetails: function(callback){
+            callback(null, {name: this.session.identity.data.name || this.session.identity.data.screen_name});
+        }
+    }}))
 	.use(connect.static(__dirname + '/static')
 );
+
+for(var i = 0, l = config.identityProviders.length; i < l; i++){
+    authom.createServer(config.identityProviders[i]);
+}
+
+authom.on('auth', function(req, res, data) {
+    req.session.hasIndentity = true;
+    req.session.identity = data;
+    res.writeHead(302, {
+        'Location': '/editor/index.html'
+    });
+    res.end();
+});
+
+authom.on('error', function(req, res, data) {
+    res.writeHead(302, {
+        'Location': '/signin.html'
+    });
+    res.end();
+});
+
+authom.listen(server);
 server.listen(8001);
 
-var server = connect();
-var io = socketio.listen(8002); 
-var nicknames = {};
+var io = socketio.listen(8002);
 var waitingGame = null;
 
 io.set('log level', 0);
