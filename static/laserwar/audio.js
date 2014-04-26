664fc5edd93000b08a9f3eed29db14c2934269c6
ewoudj <ewoudj@gmail.com>
2014-04-27 01:11:31 +0200

    Code clean up and removal of unwanted globals

--- static/laserwar/audio.js
+++ static/laserwar/audio.js
@@ -1,11 +1,5 @@
-//soundManager.debugMode = false;
-//soundManager.preferFlash = true;
-//soundManager.useHTML5Audio = true;
-//soundManager.url = 'resources/script/soundmanager/swf/';
-//soundManager.onready( function() {
   window.audio = {
-		//volume				: engine.effectsVolume,
-		setVolume			: function(newVolume){
+		setVolume: function(newVolume){
 			audio.volume = newVolume;
 			for(var s in audio){
 				if(audio[s].setVolume){
@@ -13,100 +7,70 @@
 				}
 			}
 		},
-		decreaseVolume		: function(){
+		decreaseVolume: function(){
 			if(audio.volume > 0){
 				audio.volume -= 10;
 			}
 			audio.setVolume(audio.volume);
 		},		
-		increaseVolume		: function(){
+		increaseVolume: function(){
 			if(audio.volume < 100){
 				audio.volume += 10;
 			}
 			audio.setVolume(audio.volume);
 		},		
-		mute				: function(){
+		mute: function(){
 			audio.volume = 0;
 			audio.setVolume(audio.volume);
-		},
-		iOS					: function(){
-			return (
-					//(navigator.userAgent.match(/iPhone/i)) || 
-					(navigator.userAgent.match(/iPod/i)) || 
-					(navigator.userAgent.match(/iPad/i)) 
-			);
-		}//,
-//		getAudio			: function(id, url){
-//			var result = !audio.iOS() ? soundManager.createSound({id:id, url:url, volume: engine.effectsVolume}) : null;
-//			// In some cases it is just not going to work (e.g old IE without flash)
-//			// createSound will return false.
-//			if(!result){
-//				// For now we return a place holder object
-//				result = {
-//					play: function(){},
-//					setVolume: function(){}
-//				};
-//			}
-//			return result;
-//		}
+		}
 };
 
-//	audio.explosionAudio 	= audio.getAudio('explosion', 'resources/audio/effects/explosion.mp3');
-//	audio.appearAudio 		= audio.getAudio('appear', 'resources/audio/effects/appear.mp3');
-//	audio.changeColorAudio	= audio.getAudio('changecolor', 'resources/audio/effects/changecolor.mp3');
-//	audio.laserAudio 		= audio.getAudio('laserSound', 'resources/audio/effects/laser.mp3');
-	
-//});
+(function(){
+    if (!window.AudioContext) {
+        window.AudioContext = window.webkitAudioContext;
+    }
+    var audioContext = new AudioContext();
 
-if (! window.AudioContext) {
-    window.AudioContext = window.webkitAudioContext;
-}
-var audioContext = new AudioContext();
+    function loadSound(url, ctx, onSuccess, onError) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer'; // Important trick
+        request.onload = function(){
+            ctx.decodeAudioData(
+                request.response,
+                function(buffer) {
+                    onSuccess && onSuccess(buffer);
+                },
+                function(){
+                    onError && onError();
+                }
+            );
+        };
+        request.send();
+    }
 
-function loadSound(url, ctx, onSuccess, onError) {
-    // init request
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer'; // <= here secret sauce!
+    function playSound(ctx, buffer) {
+        var source = ctx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(ctx.destination);
+        source.start(0);
+    }
 
-    // function called once data is loaded
-    request.onload = function(){
-        // request.response === encoded... so decode it now
-        ctx.decodeAudioData(request.response, function(buffer) {
-            onSuccess && onSuccess(buffer);
-	}, function(){
-            onError && onError();
+    function initSound(url){
+        var result = {
+            play: function(){},
+            setVolume: function(){}
+        };
+        loadSound(url, audioContext, function(buffer){
+            result.play = function(){
+                playSound(audioContext, buffer);
+            };
         });
-    };
+        return result;
+    }
 
-    request.send();	// Start the request
-}
-
-function playSound(ctx, buffer) {
-    // create a 'bufferSource' node
-    var source = ctx.createBufferSource();
-    // setup the buffer
-    source.buffer = buffer;
-    // connect it to context destination
-    source.connect(ctx.destination);
-    // play immediatly
-    source.start(0);
-}
-
-function initSound(url){
-	var result = {
-		play: function(){},
-		setVolume: function(){}
-	};
-	loadSound(url, audioContext, function(buffer){
-		result.play = function(){
-			playSound(audioContext, buffer);
-		};
-	});
-	return result;
-}
-
-audio.explosionAudio 	= initSound('resources/audio/effects/explosion.mp3');
-audio.appearAudio 		= initSound('resources/audio/effects/appear.mp3');
-audio.changeColorAudio	= initSound('resources/audio/effects/changecolor.mp3');
-audio.laserAudio 		= initSound('resources/audio/effects/laser.mp3');
+    audio.explosionAudio 	= initSound('resources/audio/effects/explosion.mp3');
+    audio.appearAudio 		= initSound('resources/audio/effects/appear.mp3');
+    audio.changeColorAudio	= initSound('resources/audio/effects/changecolor.mp3');
+    audio.laserAudio 		= initSound('resources/audio/effects/laser.mp3');
+})();
